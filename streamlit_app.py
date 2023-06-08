093d56a0c43c0532adc3e385eea0fbcc09576016
luisbarriosgh <135397523+luisbarriosgh@users.noreply.github.com>
2023-06-08 19:28:46 -0400

    Update streamlit_app.py
    moving stop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,14 +64,14 @@ def get_fruit_load_list():
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+  
+# don't run anything past here while we troubleshoot
+streamlit.stop()
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
